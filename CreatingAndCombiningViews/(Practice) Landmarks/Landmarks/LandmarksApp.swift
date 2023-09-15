c26287440178c227fca2bf957fa42e6f16256588
Henrykim1111 <henrykim9319@gmail.com>
2023-09-15 23:21:43 +0900

    [feat] git initiation

---
+++ CreatingAndCombiningViews/(Practice) Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Hyungmin Kim on 2023/08/26.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
