ff65ac9bd08144ea18a3ecceeaa37e1878cde50d
Henrykim1111 <henrykim9319@gmail.com>
2023-09-19 17:54:17 +0900

    [feat] Section2 finished

---
+++ BuildingListsAndNavigation/StartingPoint/Landmarks/Model/LandmarkRow.swift
@@ -0,0 +1,29 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Hyungmin Kim on 2023/09/19.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow(landmark: landmarks[0])
+    }
+}
